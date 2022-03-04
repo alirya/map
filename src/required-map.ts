99396eddeeedae742a7738913c0b7aff5a24e767
dikac <rigel234@yahoo.com>
2022-03-04 22:16:08 +0700

    add required map

---
+++ src/required-map.ts
@@ -0,0 +1,8 @@
+/**
+ * Map wich will not return undefined and provide default value
+ */
+export default interface RequiredMap<Key, Value>  extends Map<Key, Value> {
+
+    get(key: Key): Value;
+
+}
